55a525b58636e91612ff4922215d402a700cef17
Medforn <pickled.spicy.cucumbers@gmail.com>
2022-11-05 03:59:27 +0400

    feat(header): header layout

--- src/common/components/header/index.tsx
+++ src/common/components/header/index.tsx
@@ -3,6 +3,7 @@ import { Link, NavLink } from "react-router-dom";
 import Modal from "../../modals/base";
 import { ReactComponent as LogoIcon } from "../../../assets/icons/logo.svg";
 import { ReactComponent as PersonIcon } from "../../../assets/icons/person.svg";
+import styles from "./styles.module.scss";
 
 const Header = () => {
   const { open } = useModal();
@@ -10,25 +11,17 @@ const Header = () => {
   const onClick = () => open(<Modal />);
 
   return (
-    <div className="container max-w-7xl h-20 mx-auto px-4 py-2 columns-2 flex justify-between items-center font-mono">
-      <div className="flex justify-start lg:w-0 lg:flex-1">
-        <Link to="/" className="object-fill shrink-0 flex items-center">
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <Link to="/">
           <LogoIcon />
         </Link>
       </div>
 
-      <div className="flex justify-end items-center space-x-6 text-white">
-        <NavLink
-          to="/docs"
-          className="text-base font-medium hover:text-blue-400"
-        >
-          Docs
-        </NavLink>
+      <div className={styles.navigation}>
+        <NavLink to="/docs">Docs</NavLink>
 
-        <PersonIcon
-          className="h-8 w-8 cursor-pointer hover:text-blue-400"
-          onClick={onClick}
-        />
+        <PersonIcon onClick={onClick} />
       </div>
     </div>
   );
