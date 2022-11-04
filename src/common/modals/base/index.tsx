2a78264c552b0d231eafe4b319c8265cbddb8f2b
Medforn <pickled.spicy.cucumbers@gmail.com>
2022-11-05 03:42:01 +0400

    feat(modal): base modal layout

--- src/common/modals/base/index.tsx
+++ src/common/modals/base/index.tsx
@@ -1,27 +1,32 @@
 import { Dialog } from "@headlessui/react";
+import { FC, PropsWithChildren } from "react";
+import styles from "./styles.module.scss";
+import { ReactComponent as CloseIcon } from "@assets/icons/close-icon.svg";
 import useModal from "@hooks/use-modal";
 
-const Modal = () => {
+interface IBaseModal extends PropsWithChildren {
+  title?: string;
+  description?: string;
+}
+
+const BaseModal: FC<IBaseModal> = ({ title, description, children }) => {
   const { close, isModalVisible } = useModal();
 
   return (
-    <Dialog open={isModalVisible} onClose={close}>
-      <Dialog.Panel>
-        <Dialog.Title>Deactivate account</Dialog.Title>
-        <Dialog.Description>
-          This will permanently deactivate your account
-        </Dialog.Description>
-
-        <p>
-          Are you sure you want to deactivate your account? All of your data
-          will be permanently removed. This action cannot be undone.
-        </p>
-
-        <button onClick={close}>Deactivate</button>
-        <button onClick={close}>Cancel</button>
+    <Dialog open={isModalVisible} onClose={close} className={styles.dialog}>
+      <Dialog.Panel className={styles.panel}>
+        <Dialog.Title className={styles.title}>
+          {title} <CloseIcon onClick={close} className={styles.close} />
+        </Dialog.Title>
+        {description && (
+          <Dialog.Description className={styles.description}>
+            {description}
+          </Dialog.Description>
+        )}
+        {children}
       </Dialog.Panel>
     </Dialog>
   );
 };
 
-export default Modal;
+export default BaseModal;
